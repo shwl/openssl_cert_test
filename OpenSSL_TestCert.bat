c2d983df98b6364a027c32dcc4302911fdf83edb
huanzhen <huanzhen@tsign.cn>
2018-12-11 17:35:33 +0800

    用户证书实例

--- OpenSSL_TestCert.bat
+++ OpenSSL_TestCert.bat
@@ -1,18 +1,32 @@
 %~d0
 cd %~dp0
-makecert -sky signature -a sha256 -n "CN=Test;O=TestO;L=杭州市;S=浙江省;C=CN;DC=DCTest" -iv ca_test.pvk -ic ca_test.cer -sv test.pvk test.cer
 
-cert2spc test.cer  test.spc
-pvk2pfx -pvk  test.pvk -spc  test.spc -pfx  test.pfx
+cd cert
 
+rem 重命名旧文件;
 set y=%date:~0,4%
 set m=%date:~5,2%
 set d=%date:~8,2%
 set t=%time: =0% 
 set h=%t:~0,2%
 set mm=%t:~3,2%
-ren test.cer test_%y%%m%%d%_%h%%mm%.cer
-ren test.pvk test_%y%%m%%d%_%h%%mm%.pvk
-ren test.spc test_%y%%m%%d%_%h%%mm%.spc
-ren test.pfx test_%y%%m%%d%_%h%%mm%.pfx
+ren user_rsa_private.key		user_rsa_private_%y%%m%%d%_%h%%mm%.key
+ren user_cert.csr				user_cert_%y%%m%%d%_%h%%mm%.csr
+ren user_cert.crt				user_cert_%y%%m%%d%_%h%%mm%.crt
+ren user.pfx					user_%y%%m%%d%_%h%%mm%.pfx
 
+cd ..
+
+rem 生成私玥;
+openssl genrsa -aes256 -passout pass:111111 -out cert\user_rsa_private.key 2048
+
+rem 生成证书请求;
+openssl req -utf8 -new -days 365 -key cert\user_rsa_private.key -passin pass:111111 -out cert\user_cert.csr -config user.cnf
+
+rem 中级证书签发下级证书;
+openssl ca -utf8 -in cert\user_cert.csr -config user.cnf -days 365 -out cert\user_cert.crt -cert cert\ca_cert.crt -keyfile cert\ca_rsa_private.key -passin pass:111111 -batch
+
+rem 转换文pfx格式证书;
+openssl pkcs12 -export -inkey cert\user_rsa_private.key -passin pass:111111 -in cert\user_cert.crt -out cert\user.pfx -passout pass:111111
+
+pause
